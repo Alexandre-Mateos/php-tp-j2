2d5099589c3902b09632cafc938e6d07e6f84531
alexandre-mateos <mateosalexandre95@gmail.com>
2025-06-19 10:54:33 +0200

    exo1

---
+++ exos_associatifs/exo1.php
@@ -0,0 +1,11 @@
+<?php
+$personne = [
+      'nom' => 'Mateos',
+      'prenom' => 'Alexandre',
+      'age' => 29
+];
+
+echo "Nom : " . $personne['nom'] . "<br>";
+echo "Prenom : ".$personne['prenom'] . "<br>";
+echo "Age : ".$personne['age']."<br>";
+?>
